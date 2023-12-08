d98735d95fd57a26775dd35510b6ec6039deb66d
Matt <mattiregojos@gmail.com>
2023-12-08 23:31:28 +0000

    Add and modify files

--- repo-update.sh
+++ repo-update.sh
@@ -1,8 +1,8 @@
 # model-deployment repo
 cp -rf git-push.sh ~/GitOps-on-GCP
-cp -rf environment-variables.sh ~/GitOps-on-GCP/environment-varibales.sh
+cp -rf environment-variables.sh ~/GitOps-on-GCP/environment-variables.sh
 cp -rf app-deployment.sh ~/GitOps-on-GCP/infrastructure-automation.sh
-cp -rf cleanup.sh ~/GitOps-on-GCP/infrastructure-automation.sh
+cp -rf cleanup.sh ~/GitOps-on-GCP/cleanup.sh
 
 # App
 cp -rf app-deployment/* ~/GitOps-on-GCP/app
