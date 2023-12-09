563d9bdc1770de40adecb883135bafb9df500c3b
Matt <mattiregojos@gmail.com>
2023-12-09 14:08:35 +0000

    Add and modify files

--- GitOps-on-GCP.sh
+++ GitOps-on-GCP.sh
@@ -39,7 +39,6 @@ argocd admin initial-password -n argocd
 # argocd app create app --repo https://github.com/mregojos/GitOps-on-GCP.git --path manifest --dest-server https://kubernetes.default.svc --dest-namespace default
 # or Create Apps Via UI
 
-
 # Build the image and push to the hub
 cd app
 docker build -t $DOCKER_USERNAME/app .
@@ -54,46 +53,7 @@ docker push $DOCKER_USERNAME/app
 # Docker logout
 docker logout
 
-# Option A: Create app.yaml manifest
-# Deploy locally (kubectl)
-cd manifest
-sh app.sh
-cd ..
-# Create app namespace
-kubectl create namespace app
-# Apply deployment
-kubectl apply -f manifest/app.yaml -n app
-kubectl get all -n app
-watch kubectl get all -n app
-kubectl expose deployment.apps/app-deployment -n app
-kubectl get all -n app
-kubectl port-forward service/app-deployment 9000:9000 --address 0.0.0.0 -n app
-# Go to <IP>:9000
-kubectl get pods -n app
-# Delete namespace
-kubectl delete namespace app
-
-# GitOps App
-# Create an application from a git repository
-# Create Apps Via CLI
-# Create app.yaml manifest
-cd manifest
-sh app.sh
-cd ..
-# Remove comment tags in git-push and add after
-kubectl port-forward svc/argocd-server -n argocd 8000:443 --address 0.0.0.0
-kubectl config set-context --current --namespace=argocd
-argocd app create app --repo https://github.com/mregojos/GitOps-on-GCP.git --path manifest --dest-server https://kubernetes.default.svc --dest-namespace default --revision main --sync-policy auto
-watch kubectl get all -n default
-kubectl expose deployment.apps/app-deployment -n default
-kubectl get all -n default
-kubectl port-forward service/app-deployment 9000:9000 --address 0.0.0.0 -n default
-# Delete App
-argocd app delete app -y
-
-# or Create Apps Via UI
-
-# Option B: Create app.yaml manifest with secrets
+# Option A: Create app.yaml manifest with secrets
 # Deploy locally (kubectl)
 kubectl delete namespace app
 # Create app namespace
@@ -133,7 +93,45 @@ argocd app delete app -y
 
 # or Create Apps Via UI
 
+###############################################################################
+# Another Option: Create app.yaml manifest
+# Deploy locally (kubectl)
+cd manifest
+sh app.sh
+cd ..
+# Create app namespace
+kubectl create namespace app
+# Apply deployment
+kubectl apply -f manifest/app.yaml -n app
+kubectl get all -n app
+watch kubectl get all -n app
+kubectl expose deployment.apps/app-deployment -n app
+kubectl get all -n app
+kubectl port-forward service/app-deployment 9000:9000 --address 0.0.0.0 -n app
+# Go to <IP>:9000
+kubectl get pods -n app
+# Delete namespace
+kubectl delete namespace app
 
+# GitOps App
+# Create an application from a git repository
+# Create Apps Via CLI
+# Create app.yaml manifest
+cd manifest
+sh app.sh
+cd ..
+# Remove comment tags in git-push and add after
+kubectl port-forward svc/argocd-server -n argocd 8000:443 --address 0.0.0.0
+kubectl config set-context --current --namespace=argocd
+argocd app create app --repo https://github.com/mregojos/GitOps-on-GCP.git --path manifest --dest-server https://kubernetes.default.svc --dest-namespace default --revision main --sync-policy auto
+watch kubectl get all -n default
+kubectl expose deployment.apps/app-deployment -n default
+kubectl get all -n default
+kubectl port-forward service/app-deployment 9000:9000 --address 0.0.0.0 -n default
+# Delete App
+argocd app delete app -y
+
+# or Create Apps Via UI
 
 
 ########## Cleanup GitOps
