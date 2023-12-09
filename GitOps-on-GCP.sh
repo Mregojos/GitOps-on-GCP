d1ddf7322cd3bbd1aaae86ec9f33c9f4bfca9323
Matt <mattiregojos@gmail.com>
2023-12-09 00:31:27 +0000

    Add and modify files

--- GitOps-on-GCP.sh
+++ GitOps-on-GCP.sh
@@ -1,5 +1,10 @@
 # GitOps
 
+# GitOps Environment Variables
+echo "USERNAME:"
+read -s DOCKER_USERNAME
+
+
 # Infrastructure
 source env*
 sh infra*
@@ -7,3 +12,14 @@ sh infra*
 # kubectl and minikube
 sh GitOps.sh
 
+# Build the image and ush to the repository
+cd app
+docker build -t $DOCKER_USERNAME/app .
+cd ..
+# hub.docker.com/repositories
+# Login Docker
+docker login
+# Docker Images
+docker images
+# Push to Docker Hub
+docker push $DOCKER_USERNAME/app
